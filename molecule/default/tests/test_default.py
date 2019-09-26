9fbc77ae3c65b9f17d95514a8503c9a954294ada
unxnn <unxnn@users.noreply.github.com>
2019-09-26 14:37:16 +0300

    Update meta and tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,3 +12,22 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_mysql_port(host):
+    mysql = host.addr("127.0.0.1")
+    mysql.port(25).is_reachable
+
+
+def test_postfix_file(host):
+    f = host.file('/etc/postfix/main.cf')
+    assert f.exists
+    assert f.is_file
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert f.mode == 0o644
+
+
+def test_postfix_is_running(host):
+    service = host.service('postfix')
+    assert service.is_running
